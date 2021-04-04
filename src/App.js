be2d3bd0cad81a1b266d47ec87a6348077596d35
goody <goody@goodyui-MacBookAir.local>
2021-04-04 20:50:39 +0900

    Add : react component class

--- src/App.js
+++ src/App.js
@@ -1,71 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    ratings : 5
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    ratings : 4.9
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    ratings: 3.2
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    ratings: 4
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    ratings: 1.5
+
+class App extends React.Component{
+  
+  state = {
+    count : 0
+  };
+
+  add = () => {
+    console.log("add");
+    this.setState(current => ({count : ++current.count}));    // setState를 호출할 때마다 리액트는 자동으로 render 함수를 실행시키게 되어있다.
   }
-];
+  
+  minus = () => {
+    console.log('minus');
+    this.setState(current => ({count : --current.count}));
+  }
 
-function Food({ name, image, ratings }) {
-  return (
-    <div className="Food">
-      <h2>I like {name}</h2>
-      <h4>{ratings}/5.0</h4>
-      <img src = {image} alt = {name}></img>
-    </div>
-  )
-}
+  componentDidMount(){
+    console.log("components mounted");
+  }
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image : PropTypes.string.isRequired,
-  ratings : PropTypes.number.isRequired
-}
+  componentDidUpdate(){
+    console.log("components updated");
+  }
 
-function renderFood(food) {
-    return (<Food key = {food.id} name={food.name} image = {food.image} ratings = {food.ratings}/>)
-}
+  render() {
+    console.log('components rendering');
+    return(
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 
-
-function App() {
-  return (
-    <div className="App">
-      {foodILike.map(dish => renderFood(dish))}
-    </div>
-  );
 }
 
 export default App;
